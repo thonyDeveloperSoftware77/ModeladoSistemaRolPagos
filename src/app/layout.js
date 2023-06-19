0e75e49e218218d75bb734fef4f5f8e4caeff682
Anthony Cochea <thonydevelopersoftware@gmail.com>
2023-06-19 09:19:36 -0500

    segundo commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"></meta>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
